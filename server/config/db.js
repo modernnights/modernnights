25d2626815a947e4f7c7742d16bb57f0a543a595
Jake Shasteen <jakeshasteen@gmail.com>
2016-04-13 20:28:35 -0400

    Add SQL port environment variable

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,17 @@ var Sequelize = require( 'sequelize' );
 
 var user = process.env.SQLUSER || null;
 var password = process.env.SQLPASS || null;
+var port = process.env.SQLPORT || 1433;
 
 var db = new Sequelize( 'muxdata', user, password, {
   host: 'localhost',
+  port: port,
   dialect: 'mysql',
 
   define: {
     underscored: true
   }
-  
+
 });
 
 module.exports = db;
