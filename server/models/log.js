84786c6c6358892ec4a93f6cad2813919961a4b8
Jake Shasteen <jakeshasteen@gmail.com>
2016-04-15 09:18:19 -0400

    Write Log model

--- server/models/log.js
+++ server/models/log.js
@@ -2,11 +2,15 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Log = sequelize.define("Log", {
-    name: DataTypes.STRING
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    xp_change: DataTypes.INTEGER,
   }, {
     classMethods: {
       associate: function( models ) {
         Log.belongsTo( models.Character );
+        Log.belongsTo( models.Player );
+        Log.belongsTo( models.Player, {as: 'approved_by'} );
       }
     }
   });
