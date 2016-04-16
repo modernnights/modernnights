116753f5ba802aea729da395c6526c77beec228b
Jake Shasteen <jakeshasteen@gmail.com>
2016-04-16 10:24:27 -0700

    Initialize stat types

--- server/lib/initTableData.js
+++ server/lib/initTableData.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function( models ) {
-  /* Initialize Permissions */
+  /* Permissions */
   models.Permission.findOrCreate({ 
     where: {
       name: 'Mortal',
@@ -76,25 +76,90 @@ module.exports = function( models ) {
     });
   })
   // todo: bulk-add stat types
-    // attributes
-      // physical
-      // mental
-      // social
-    // abilities
-      // talents
-      // skills
-      // knowledges
-    // advantages
-      // backgrounds
-      // virtues
-      // disciplines
-      // quirks
-        // merits
-        // flaws
-
+  const types = [
+    {
+      name: 'attributes',
+      rest: [
+        { name: 'physical' },
+        { name: 'mental' },
+        { name: 'social' }
+      ]
+    },
+    {
+      name: 'abilities',
+      rest: [
+        { name: 'talents' },
+        { name: 'skills' },
+        { name: 'knowledges' }
+      ]
+    },
+    {
+      name: 'advantages',
+      rest: [
+        { name: 'backgrounds' },
+        { name: 'virtues' },
+        { 
+          name: 'powers',
+          rest: [
+            { name: 'disciplines' }
+          ]
+        },
+        {
+          name: 'quirks',
+          rest: [
+            { name: 'merits' },
+            { name: 'flaws' }
+          ]
+        }
+      ]
+    }
+  ];
+  const addTypes = function( types, prevID ) {
+    types.forEach( function( type ) {
+      models.StatType.findOrCreate({
+        where: {
+          name: type.name,
+          parent_type_id: prevID,
+        }
+      }).spread( function( instance ) {
+        if( type.rest ) {
+          addTypes( type.rest, instance.get('id') );
+        }
+      })
+    });
+  }
+  addTypes( types, null );
   // todo: bulk-add spreads
+  const spreads = [
+    [7, 5, 3],
+    [6, 4, 3],
+    [13, 9, 5],
+    [11, 7, 4],
+    [3, 0, 0],
+    [4, 0, 0],
+    [5, 0, 0],
+    [2, 0, 0]
+  ];
+  spreads.forEach( function( spread ) {
+    models.Spread.findOrCreate({
+      where: {
+        primary: spread[0],
+        secondary: spread[1],
+        tertiary: spread[2]
+      }
+    })
+  });
   // todo: bulk-add stats
   // todo: bulk-add monsters
+    // mortal
+      // werewolf
+        // subtype
+          // subtype
+      // vampire
+        // clan
+          // bloodline
+    // Where do ghoul and revenant sit?
+
   // todo: bulk-add orgs
 
 }
