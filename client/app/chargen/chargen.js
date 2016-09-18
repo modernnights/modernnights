c6cc454940855af269abab7334b148f0035f033d
Marissa Conner <talk@fairgroundcreative.com>
2016-09-18 16:56:49 -0700

    Nature|Demeanor dropdown options filled out

--- client/app/chargen/chargen.js
+++ client/app/chargen/chargen.js
@@ -3,12 +3,21 @@ angular.module( 'modernnights.chargen', [] )
 .controller( 'ChargenController', function( Character, Stat, $location, $scope ) {
   $scope.char = {};
   
+  var getArchetypes = function(){
+    Stat.getArchetypes()
+    .then( function( data ){
+      $scope.archetypes = data;
+    });
+  };
+  
   var getAttributes = function(){
     Stat.getStatByType( 'attributes' )
     .then( function( data ){
       $scope.attributes = data; 
     });
   };
+  
+  getArchetypes();
   getAttributes();
   
   $scope.chargen = function( isValid ) {
